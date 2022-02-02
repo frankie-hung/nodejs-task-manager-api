d8052dfcba5a4085f0cb921c090e81daff39aa06
Frankie Hung <airming@gmail.com>
2022-02-03 00:24:23 +0800

    udpated patch /users and delete /users endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -82,7 +82,7 @@ router.get('/users/:id', async (req, res) => {
     // })
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowUpdates.includes(update))
@@ -90,14 +90,11 @@ router.patch('/users/:id', async (req, res) => {
     if (!isValidOperation) return res.status(400).send({ error: 'Invalid update!' })
 
     try {
-        const user = await User.findById(req.params.id)
+        //const user = await User.findById(req.params.id)
+        const user = req.user
 
         updates.forEach((update) => user[update] = req.body[update])
         await user.save()
-
-        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-        if (!user) return res.status(404).send()
         res.send(user)
     } catch (e) {
         res.status(400).send(e)
@@ -105,12 +102,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
 
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) return res.status(404).send()
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
         console.log(e)
