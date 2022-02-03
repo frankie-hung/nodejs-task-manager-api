b3b13c0e353b39fe11a12ffe9b6e9db7954a4b66
Frankie Hung <airming@gmail.com>
2022-02-03 22:35:51 +0800

    added file upload for user avatar

--- src/index.js
+++ src/index.js
@@ -6,20 +6,6 @@ require('./db/mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-
-//     if (req.method === 'GET'){
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Website is currently under maintenance')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -27,18 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async()=>{
-    // const task = await Task.findById('61fab2003e8462d37c11a7b4')
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    // const user = await User.findById('61faae34e7ff2df8be5e98ba')
-    // await user.populate('tasks')
-    // console.log(user.tasks)
-}
-
-main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,8 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 const auth = require('../middleware/auth')
-
+const multer = require('multer')
+const sharp = require('sharp')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -15,11 +16,62 @@ router.post('/users', async (req, res) => {
     }
 })
 
+
+const upload = multer({
+    //dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|png|jpeg)$/)) {
+            return cb(new Error('Please upload an image file'))
+        }
+        cb(undefined, true)
+    }
+})
+
+// upload avatar image
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    //req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) =>{
+    console.log(error)
+    res.status(400).send({ error: error.message})
+})
+
+// delete avatar image
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) =>{
+    console.log(error)
+    res.status(400).send({ error: error.message})
+})
+
+// get avatar image
+router.get('/users/:id/avatar', async(req, res)=>{
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+
+    } catch (e){
+        res.status(404).send()
+    }
+})
+
 // get user profile of the authenticated user
 router.get('/users/me', auth, async (req, res) => {
-
     res.send(req.user)
-
 })
 
 router.post('/users/login', async (req, res) => {
@@ -69,17 +121,6 @@ router.get('/users/:id', async (req, res) => {
         console.log(e)
     }
 
-
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    //     console.log(e)
-
-    // })
 })
 
 router.patch('/users/me', auth, async (req, res) => {
